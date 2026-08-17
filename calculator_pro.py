05c5ecb68422f761343e942304a341a4b80c1874
Leon-AM <leon.ss.kenned@gmail.com>
2026-08-17 20:57:46 +0330

    calculator

--- calculator_pro.py
+++ calculator_pro.py
@@ -1,45 +1,51 @@
+def menu():
+    print("""
+========================
+    CALCULATOR
+========================
+
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Power
+6. Modulo
+7. Exit\n""")
+
+
+
+
+def operacion(choice , num1 , num2):
+    if choice == 1:
+        return num1 + num2
+    elif choice == 2:
+        return num1 - num2
+    elif choice == 3:
+        return num1 * num2
+    elif choice == 4:
+       return num1 / num2
+    elif choice == 5:
+        return num1 ** num2
+    elif choice == 6:
+        return num1 % num2
+
+
+
 while True:
-    def meno():
-        print("""
-    ========================
-        CALCULATOR
-    ========================
-
-    1. Add
-    2. Subtract
-    3. Multiply
-    4. Divide
-    5. Power
-    6. Modulo
-    7. Exit\n""")
-
-        choice = int(input("Choose: "))
-        
-        ent2 = float(input("num one? "))
-        ent3 = float(input("num two? "))
-        return choice , ent2 , ent3
-        
-
-    def total(choice , ent2 , ent3):
-        if choice == 1:
-            return( ent2 + ent3)
-        elif choice == 2:
-            return(ent2 - ent3)
-            
-        elif choice == 3:
-            return(ent2 * ent3)
-        elif choice == 4:
-            return(ent2 / ent3)
-        elif choice == 5:
-            return(ent2 ** ent3)
-        elif choice == 6:
-            return ent2 % ent3
-
-    while True:
-        choice, ent2, ent3 = meno()
-        if choice == 7:
-            break
     
+    menu()
+    choice = int(input("choice? "))
+    while choice >7:
+        print("bog")
+        choice = int(input("choice? "))
+        
+    if choice ==7:
+        break
+    else:
+        num1 = float(input("num1 ?"))
+        num2 = float(input("num2 ?"))
     
-    result = total(choice,ent2,ent3)
-    print(result)
+    result = operacion(choice , num1 , num2)
+    print(f"\n Result: {result}")
+
+
